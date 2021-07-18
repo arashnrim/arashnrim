b1f6fa89fef179fe02bad33a998708e07567d3c9
Arash <47273556+arashnrim@users.noreply.github.com>
2021-07-18 14:08:50 +0800

    ✨ Add automated language updating

---
+++ update/main.py
@@ -0,0 +1,66 @@
+import json
+from datetime import datetime
+import logging
+import sys
+import requests
+
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+logging.info("Attempting to update languages in the root README.md file. Initiating...")
+
+CONTENT = """
+## 👋 Hello world, I'm Arash!
+
+I'm a student with a passion for making a change through technology. While that one-liner is fairly ambiguous, it gives me a purpose and motivation to create interesting (and hopefully helpful!) stuff.
+
+---
+
+### I'm using...
+
+"""
+
+with open(".languageignore") as file:
+    ignored_projects = file.readline().split(",")
+logging.info("Read projects to ignore.")
+
+logging.info("Attempting to reach GitHub's API...")
+repos = requests.get("https://api.github.com/users/arashnrim/repos?sort=pushed")
+languages = []
+count = {}
+
+if repos.status_code != 200:
+    logging.critical("Response from GitHub's API returned a non-OK (200) status code. Stopping process for safety.")
+    sys.exit(-1)
+
+repos = json.loads(repos.text)
+logging.info("Retrieved details of repositories from GitHub's API.")
+
+logging.info("Extracting languages...")
+for repo in repos:
+    if repo["name"] in ignored_projects:
+        continue
+    if not repo["language"] is None:
+        if repo["language"] in languages:
+            count[repo["language"]] += 1
+            continue
+        else:
+            count[repo["language"]] = 1
+            languages.append(repo["language"])
+
+logging.info("Appending languages...")
+for language in languages:
+    CONTENT += "- {} (in {} project{})\n".format(language, count[language], "s" if count[language] != 1 else "")
+
+logging.info("Appending end chunk...")
+CONTENT += """
+<sub>Last updated: {}</sub>
+
+---
+
+Feel free to <a href="mailto:hello@arashnrim.me" target="_blank" rel="noreferrer">be in contact</a> — after all, everyone's learning together! Thanks for stopping by, and go create awesome things!
+""".format(datetime.today().strftime("%d %B %Y"))
+
+logging.info("Writing to file...")
+with open("../README.md", "w") as file:
+    file.write(CONTENT)
+
+logging.info("The program has completed successfully.")
